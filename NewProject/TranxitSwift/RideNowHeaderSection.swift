1976ca590bb2485efe36b8983efedbb7f0259f7f
kumaresan <kumaresan@appoets.com>
2019-05-08 02:28:29 +0530

    Coupon Added

---
+++ NewProject/TranxitSwift/RideNowHeaderSection.swift
@@ -0,0 +1,21 @@
+//
+//  RideNowHeaderSection.swift
+//  User
+//
+//  Created by CSS on 08/05/19.
+//  Copyright © 2019 Appoets. All rights reserved.
+//
+
+import UIKit
+
+class RideNowHeaderSection: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
